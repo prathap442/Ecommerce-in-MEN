a25088c81a7d6b5b3dba340c00638029c463ff43
prathap442 <pmohansaikrishna@gmail.com>
2020-04-22 11:35:40 +0530

    setted up the intial route to work

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+app.get('/',(req,res)=>{
+  res.send({"firstname": "prathap"})  
+})
+
+
+app.listen(port, function() {
+  console.log('listening on port', port); 
+})
